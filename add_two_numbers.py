6df04cdac642f62fc0fd2fb4f54b229ef95ff86e
kamiiss <zyu.zhang@outlook.com>
2020-03-06 14:11:43 +0000

    tidy up

--- add_two_numbers.py
+++ add_two_numbers.py
@@ -1,8 +1,7 @@
 # Q2
-# You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
-#
+# You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse
+# order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
 # You may assume the two numbers do not contain any leading zero, except the number 0 itself.
-
 
 
 # Definition for singly-linked list.
@@ -11,6 +10,7 @@ class ListNode(object):
         self.val = x
         self.next = None
 
+
 # Faster solution (could be better)
 def exp_ten(x):
     res = 1
@@ -18,7 +18,8 @@ def exp_ten(x):
         res *= 10
     return res
 
-def addTwoNumbers(self, l1, l2):
+
+def addTwoNumbers(l1, l2):
     """
     :type l1: ListNode
     :type l2: ListNode

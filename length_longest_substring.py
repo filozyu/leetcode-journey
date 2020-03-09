4f3cdf4b8397a6a4a662f026e0b5cec1be054199
kamiiss <zyu.zhang@outlook.com>
2020-03-09 16:17:03 +0000

    faster sol for Q3

--- length_longest_substring.py
+++ length_longest_substring.py
@@ -2,10 +2,12 @@
 # Given a string, find the length of the longest substring without repeating characters.
 
 
-def lengthOfLongestSubstring(s):
+def lengthOfLongestSubstring_one(s):
     """
-    :type s: str
-    :rtype: int
+    Brute force
+
+    Time: O(n^2)
+    Space: O(n^2) (worst)
     """
     if len(s) > 0:
         max_length = 1
@@ -26,3 +28,28 @@ def lengthOfLongestSubstring(s):
             if max_length < len(curr_list):
                 max_length = len(curr_list)
     return max_length
+
+
+def lengthOfLongestSubstring_two(s):
+    """
+    Sliding window
+
+    Time: O(n), assuming lookup key in dict using dict.get() is O(1)
+    Space: O(n), for storing the lookup dict
+    """
+    max_len = 0
+    start = end = 0
+    lookup = {}
+    for i in range(len(s)):
+        end += 1
+        if lookup.get(s[i]) and lookup[s[i]][-1] >= start:
+            # found a repeated element
+            start = lookup[s[i]][-1] + 1
+            # reset start to be the next element of the repeated element
+            lookup[s[i]].append(i)
+            # update the location dict
+        else:
+            lookup[s[i]] = [i]
+        if (end - start) > max_len:
+            max_len = end - start
+    return max_len

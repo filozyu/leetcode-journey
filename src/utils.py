aafdc508fd205e1ab6376d61833d74ed94f0f475
kamiiss <zyu.zhang@outlook.com>
2020-03-28 19:36:52 +0000

    add utils for binary tree

--- src/utils.py
+++ src/utils.py
@@ -1,14 +1,25 @@
+from collections import deque
 from time import perf_counter
 
+# Data Structure #
 
-# Data Structure
+
+# linked list
 class ListNode:
     def __init__(self, x):
         self.val = x
         self.next = None
 
 
-# Methods
+# binary tree
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+# Methods #
 def timer(num_trails, call_func, *args, **kwargs):
     start = perf_counter()
     for i in range(num_trails):
@@ -35,3 +46,44 @@ def ListNode2list(input_listnode):
         res.append(curr.val)
         curr = curr.next
     return res
+
+
+def list2binary_tree(input_list):
+    """
+    Build binary tree from the given input list
+    :param input_list: start with root, None should be inserted to indicate leaf nodes (the last layer can be inferred)
+    :return: root, TreeNode
+    """
+    # e.g. [1, 2, 3, None, None, 4, 5, 6, 7, 8]
+    #     1
+    #   /  \
+    #  2    3
+    #     /   \
+    #    4    5
+    #   /\   /
+    #  6 7  8
+    visited = deque()
+    root = TreeNode(input_list[0])
+    input_list.pop(0)
+    visited.append(root)
+    while len(input_list) > 0:
+        curr_node = visited.popleft()
+        if not curr_node:
+            continue
+        if input_list[0]:
+            left_child = TreeNode(input_list.pop(0))
+        else:
+            left_child = input_list.pop(0)
+        if len(input_list) == 0:
+            curr_node.left = left_child
+            break
+        if input_list[0]:
+            right_child = TreeNode(input_list.pop(0))
+        else:
+            right_child = input_list.pop(0)
+        curr_node.left = left_child
+        curr_node.right = right_child
+        visited.append(left_child)
+        visited.append(right_child)
+
+    return root

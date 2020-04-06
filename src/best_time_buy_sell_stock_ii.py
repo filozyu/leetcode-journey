57127d09a79ec89d1cb056b1e0975c12148a1e6d
kamiiss <zyu.zhang@outlook.com>
2020-04-06 12:44:25 +0100

    add greedy approach for Q122

--- src/best_time_buy_sell_stock_ii.py
+++ src/best_time_buy_sell_stock_ii.py
@@ -25,5 +25,22 @@ def maxProfit(prices):
     return profit
 
 
-test = [7,6,4,3,1]
+def maxProfit_greedy(prices):
+    """
+    Greedy (memory efficient)
+    Time: O(n)
+    Space: O(1)
+    """
+    profit = 0
+    for i in range(1, len(prices)):
+        # profits are all the values combined when the stock is climbing
+        # equivalent of selling and buying everyday to gain the max profit of the present day
+        # if price goes up from i-1 to i, then buy at i-1 and sell at i
+        # if price goes down from i-1 to i, then do not buy at i-1
+        # i++
+        profit += max(prices[i] - prices[i - 1], 0)
+    return profit
+
+
+test = [7, 6, 4, 3, 1]
 print(maxProfit(test))

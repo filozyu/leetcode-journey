a0f245dfd06a1f327cd23c591fd522f81018c654
kamiiss <zyu.zhang@outlook.com>
2020-05-31 17:39:44 +0100

    add one pass for Q138

--- src/copy_random_list.py
+++ src/copy_random_list.py
@@ -38,3 +38,36 @@ def copyRandomList(head):
         prev_node = prev_node.next
 
     return copy_head
+
+
+def copyRandomList_one_pass(head):
+    """
+    Iterative with a visited-copy dictionary
+    Time: O(n), one passes, copying the spine of the list and adding random links
+    Space: O(n), the dictionary
+    """
+    def check_copy_dict(copy_dict, node):
+        if node is None:
+            return None
+        if copy_dict.get(node):
+            return copy_dict[node]
+        else:
+            copy_dict[node] = Node(node.val)
+            return copy_dict[node]
+
+    if not head:
+        return None
+    prev_node = head
+    copy_dict = {}
+
+    while prev_node is not None:
+        copy_prev = check_copy_dict(copy_dict, prev_node)
+        next_node = prev_node.next
+        copy_next = check_copy_dict(copy_dict, next_node)
+        rand_node = prev_node.random
+        copy_rand = check_copy_dict(copy_dict, rand_node)
+        copy_prev.next = copy_next
+        copy_prev.random = copy_rand
+        prev_node = next_node
+
+    return copy_dict[head]
